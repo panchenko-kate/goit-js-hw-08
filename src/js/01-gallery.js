e82626bde473200baa23a387933a055e6b653494
panchenko-kate <kmaslova_007@ukr.net>
2022-12-20 21:36:11 +0200

    first exercise

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+console.log(SimpleLightbox);
+
+const imgContainer = document.querySelector('.gallery');
+const imgsCardsMarkup = createImageCardsMurkup(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', imgsCardsMarkup);
+
+function createImageCardsMurkup (imgs) {
+    return imgs.map(({preview, original, description}) => {
+        return `
+        <a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}"/>
+        </a>
+        `;
+    })
+    .join('')
+;
+};
+
+new SimpleLightbox('.gallery__item', {captionsData: 'alt', captionDelay: 250,});
