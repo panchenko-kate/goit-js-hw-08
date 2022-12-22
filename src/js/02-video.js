7a1f012c4910b5c77946c7ae48c2ca44d2b8405d
panchenko-kate <kmaslova_007@ukr.net>
2022-12-23 01:47:19 +0200

    second exercise

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,22 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
-confirm,console.log(Player);
+console.log(Player);
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = "videoplayer-current-time";
+
+player.on('timeupdate', throttle(onPlay, 1000));
+   
+function onPlay ({ seconds }) {
+    localStorage.setItem(currentTime, seconds);
+}
+
+player.setCurrentTime().then(function() {
+}).catch(function() {
+    if(!localStorage.getItem(currentTime)){
+        return;
+    }
+    player.setCurrentTime(localStorage.getItem(currentTime));
+});
